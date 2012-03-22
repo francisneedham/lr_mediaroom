a70704ee802056c4b0a4ef868ce918f8d8845c4c
Marchino <marco.crepaldi@gmail.com>
2012-03-22 08:54:33 +0100

    test importazione immagini con exif e senza

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,30 @@
+# encoding: UTF-8
+require 'mini_exiftool'
+
 class Photo < ActiveRecord::Base
   belongs_to :gallery
   mount_uploader :image, ::PhotoUploader
+
+  EXIF_DATA_MAPPING = {
+    'description' => 'title'
+  }
+
+  def self.import_from_path(path)
+    file_data = MiniExiftool.new path, :convert_encoding => true
+    EXIF_DATA_MAPPING.each do |exif_tag, field|
+      if file_data[exif_tag].present?
+        field_value = file_data[exif_tag]
+      else
+        field_value = path.split('/').last
+      end
+      image = self.new
+      image.send("#{field}=".to_sym, field_value)
+      if image.save
+        return image
+      else
+        return false
+      end
+    end
+  end
+
 end
