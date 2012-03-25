a02868288529e2f41013e917b2105b5aec6b1f81
Marchino <marco.crepaldi@gmail.com>
2012-03-25 14:48:52 +0200

    fixati test

--- test/unit/gallery_test.rb
+++ test/unit/gallery_test.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../../test_config', __FILE__)
+require 'test_helper'
 require 'minitest/autorun'
 
 class GalleryTest < ActiveSupport::TestCase
--- test/unit/photo_test.rb
+++ test/unit/photo_test.rb
@@ -1,20 +1,20 @@
 # encoding: UTF-8
-require File.expand_path('../../test_config', __FILE__)
+require 'test_helper'
 require 'minitest/autorun'
 
 class PhotoTest < ActiveSupport::TestCase
   
-  def import_from_path_without_exif_data
-    path = 'assets/no_exif.png'
+  def test_import_from_path_without_exif_data
+    path = 'test/assets/no_exif.png'
     assert_difference 'Photo.count', 1 do
       image = Photo.import_from_path path
       assert 'no_exif.png', image.title
     end
   end
 
-  def import_from_path_with_exif_data
-    path = 'assets/exif.jpg'
-    assert_difference 'Photo.all', 1 do
+  def test_import_from_path_with_exif_data
+    path = 'test/assets/exif.jpg'
+    assert_difference 'Photo.count', 1 do
       image = Photo.import_from_path path
       assert 'Pradello, Bellano, 29/01/12 Campanile della chiesa di Pradello Photo: © Carlo Borlenghi', image.title
     end
