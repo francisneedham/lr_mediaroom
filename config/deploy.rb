77d07723343ea77ba2dd3982f664bfe441a18335
Marchino <marco.crepaldi@gmail.com>
2012-04-03 13:54:32 +0200

    symlinks per uplaods

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,6 +54,8 @@ namespace :deploy do
     run "ln -nfs #{shared_path}/media/videos #{release_path}/videos"
     run "rm -fr #{release_path}/galleries"
     run "ln -nfs #{shared_path}/media/galleries #{release_path}/galleries"
+    run "rm -fr #{release_path}/public/uploads"
+    run "ln -nfs #{shared_path}/uploads #{release_path}/public/uplaods"
   end
   after "deploy:finalize_update", "deploy:symlink_media_files"
 
