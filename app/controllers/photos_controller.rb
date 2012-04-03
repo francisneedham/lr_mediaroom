38d52cf36c3fc4e18009f20b4e243a0f8b9ace6f
Marchino <marco.crepaldi@gmail.com>
2012-04-03 15:39:33 +0200

    ordinamento foto opposto

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,7 +4,7 @@ class PhotosController < ApplicationController
     @gallery = Gallery.find(params[:gallery_id])
     @photos = @gallery.photos
                       .published
-                      .order('id DESC')
+                      .order('id ASC')
                       .page(params[:page]).per(20)
   end
 
